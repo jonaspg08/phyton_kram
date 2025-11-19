ed7ad40aefbcaffba5d1ae6683bc0f33f7632cd3
Jonas G. <jp08gim@gmail.com>
2025-11-19 09:57:48 +0100

    bug fixes

--- scooteqAnwendung.py
+++ scooteqAnwendung.py
@@ -1,25 +1,26 @@
 kilometer = 1
-zeit = 6
+zeit = 0.16
+grundgebühr = 1.5
 
 
 while True:
-    wahl = input("Welche Variable soll verwendet werden? (kilometer oder zeit, 'stop' zum Beenden): ")
+    wahl = input("Wie willst du deine Rechnung berechnen? (km für Kilometer, Time für Zeit oder 'stop' zum Beenden): ")
 
-    if wahl == "kilometer":
+    if wahl.lower() == "km":
         # Benutzer gibt eine Zahl ein, die mit kilometer multipliziert wird
         zahl = float(input("Gib deine gefahrene Kilometerzahl ein: "))
-        ergebnis = kilometer * zahl
-        print(f"Dein Endbetrag beträgt: {ergebnis}€")
+        ergebnis = kilometer * zahl + grundgebühr
+        print(f"Dein Endbetrag beträgt: {round(ergebnis, 2)}€")
 
-    elif wahl == "zeit":
+    elif wahl.lower() == "time":
         # Benutzer gibt eine Zahl ein, die mit zeit multipliziert wird
         zahl = float(input("Gib deine gefahrene Zeit in Minuten ein: "))
-        ergebnis = zeit * zahl
-        print(f"Dein Endbetrag beträgt: {ergebnis}€")
+        ergebnis = zeit * zahl + grundgebühr
+        print(f"Dein Endbetrag beträgt: {round(ergebnis, 2)}€")
 
-    elif wahl == "stop":
+    elif wahl.lower() == "stop":
         print("Programm beendet.")
         break
 
     else:
-        print("Ungültige Eingabe! Bitte 'kilometer', 'zeit' oder 'exit' eingeben.")
+        print("Ungültige Eingabe! Bitte 'km', 'time' oder 'stop' eingeben.")
